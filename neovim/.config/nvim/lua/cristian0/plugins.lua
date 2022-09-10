949be5e7f1ee0195401422063cc79ed4d0423395
Cris <ctovar@abexuspr.com>
2022-09-10 07:54:14 -0500

    neovim>telescope extensions

--- neovim/.config/nvim/lua/cristian0/plugins.lua
+++ neovim/.config/nvim/lua/cristian0/plugins.lua
@@ -66,7 +66,14 @@ return packer.startup(function(use)
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
 
   -- Telescope
-  use "nvim-telescope/telescope.nvim"
+  use {
+    "nvim-telescope/telescope.nvim",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "BurntSushi/ripgrep",
+      { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
+    }
+  }
 
   -- Treesitter
   use {
