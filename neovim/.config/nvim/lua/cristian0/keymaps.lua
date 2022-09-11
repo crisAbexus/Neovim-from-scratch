9bd0f58a670a606f7e8d5c7f2a4199305523dcba
Cris <ctovar@abexuspr.com>
2022-09-11 06:36:57 -0500

    neovim>keymaps new shortcut ñ0 to remove highlight

--- neovim/.config/nvim/lua/cristian0/keymaps.lua
+++ neovim/.config/nvim/lua/cristian0/keymaps.lua
@@ -20,26 +20,26 @@ vim.g.maplocalleader = "ñ"
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-h>", "<C-w>h", opts);
+keymap("n", "<C-j>", "<C-w>j", opts);
+keymap("n", "<C-k>", "<C-w>k", opts);
+keymap("n", "<C-l>", "<C-w>l", opts);
 
 --[[ keymap("n", "<leader>e", ":Lex 30<cr>", opts) ]]
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-Up>", ":resize -2<CR>", opts);
+keymap("n", "<C-Down>", ":resize +2<CR>", opts);
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts);
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts);
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", ":bnext<CR>", opts);
+keymap("n", "<S-h>", ":bprevious<CR>", opts);
 
 -- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts);
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts);
 
 -- Insert Mode --
 -- Press jk fast to enter
@@ -47,45 +47,46 @@ keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
 -- Visual --
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+keymap("v", "<", "<gv", opts);
+keymap("v", ">", ">gv", opts);
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
+keymap("v", "<A-j>", ":m .+1<CR>==", opts);
+keymap("v", "<A-k>", ":m .-2<CR>==", opts);
+keymap("v", "p", '"_dP', opts);
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts);
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts);
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts);
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts);
 
 -- Terminal --
 -- Better terminal navigation
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
-keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts);
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts);
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts);
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts);
 
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<c-g>",
   "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>",
-  opts)
-keymap("n", "<c-f>", "<cmd>Telescope live_grep<cr>", opts)
+  opts);
+keymap("n", "<c-f>", "<cmd>Telescope live_grep<cr>", opts);
 
 -- Nvimtree
-keymap("n", "¿e", ":Neotree toggle<cr>", opts)
-keymap("n", "ñe", ":Neotree toggle<cr>", opts)
+keymap("n", "¿e", ":Neotree toggle<cr>", opts);
+keymap("n", "ñe", ":Neotree toggle<cr>", opts);
 
 -- buffers
 -- Close buffer
-keymap("n", "¿q", ":Bdelete<CR>", opts)
-keymap("n", "ñq", ":Bdelete<CR>", opts)
+keymap("n", "¿q", ":Bdelete<CR>", opts);
+keymap("n", "ñq", ":Bdelete<CR>", opts);
 
 -- Close pane
-keymap("n", "ñx", ":clo<CR>", opts)
-keymap("n", "¿x", ":clo<CR>", opts)
-keymap("n", "ñ|", "<C-w>v", opts)
-keymap("n", "<leader>-", "<C-w>s", opts)
+keymap("n", "ñx", ":clo<CR>", opts);
+keymap("n", "¿x", ":clo<CR>", opts);
+keymap("n", "ñ|", "<C-w>v", opts);
+keymap("n", "<leader>-", "<C-w>s", opts);
+keymap("n", "ñ0", "<cmd>noh<cr>", opts);
