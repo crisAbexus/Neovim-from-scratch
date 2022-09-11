0ca66ee463e14edff0303dce2d9610bc7159f57f
Cris <ctovar@abexuspr.com>
2022-09-11 07:09:56 -0500

    neovim>keymaps for choose among options

--- neovim/.config/nvim/lua/cristian0/keymaps.lua
+++ neovim/.config/nvim/lua/cristian0/keymaps.lua
@@ -90,3 +90,36 @@ keymap("n", "¿x", ":clo<CR>", opts);
 keymap("n", "ñ|", "<C-w>v", opts);
 keymap("n", "<leader>-", "<C-w>s", opts);
 keymap("n", "ñ0", "<cmd>noh<cr>", opts);
+
+-- luasnip
+local ls = require("luasnip")
+
+vim.keymap.set({ "i", "s" }, "<c-p>", function()
+  if ls.expand_or_jumpable() then
+    ls.expand()
+  end
+end)
+
+vim.keymap.set({ "i", "s" }, "<c-k>", function()
+  if ls.jumpable(1) then
+    ls.jump(1)
+  end
+end)
+
+vim.keymap.set({ "i", "s" }, "<c-j>", function()
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
+end)
+
+vim.keymap.set({ "i", "s" }, "<c-l>", function()
+  if ls.choice_active() then
+    ls.change_choice(1)
+  end
+end)
+
+vim.keymap.set({ "i", "s" }, "<c-h>", function()
+  if ls.choice_active() then
+    ls.change_choice(-1)
+  end
+end)
