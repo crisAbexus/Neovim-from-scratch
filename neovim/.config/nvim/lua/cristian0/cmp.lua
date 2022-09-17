7f46cc3e84b24e833d309da5ee53e1f19a897800
cris <ctovar@abexuspr.com>
2022-09-17 17:37:10 -0500

    luasnip>prioritize snippets over lsp suggestions

--- neovim/.config/nvim/lua/cristian0/cmp.lua
+++ neovim/.config/nvim/lua/cristian0/cmp.lua
@@ -100,8 +100,8 @@ cmp.setup {
       vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
-        nvim_lsp = "[LSP]",
         luasnip = "[Snippet]",
+        nvim_lsp = "[LSP]",
         buffer = "[Buffer]",
         path = "[Path]",
       })[entry.source.name]
@@ -109,8 +109,8 @@ cmp.setup {
     end,
   },
   sources = {
-    { name = "nvim_lsp" },
     { name = "luasnip" },
+    { name = "nvim_lsp" },
     { name = "buffer" },
     { name = "path" },
   },
