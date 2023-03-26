aa0a5605092319ca34e2780eb56882f5902576b6
cris <ctovar@abexuspr.com>
2023-03-26 06:34:32 -0500

    🌳 neovim>fix the neo-tree hidden files by default:
    in case that files are hidden by default press:
    press H or in other words: <SHIFT> - h
    

---
+++ neovim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = true,
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
+  },
+}
