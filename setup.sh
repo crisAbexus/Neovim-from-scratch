65502548eab722229ca838064d68b8ff26757206
cris <ctovar@abexuspr.com>
2023-03-27 04:51:45 -0500

    😐 remove the extra tmux tab in dotfiles startup:
    Now with this configuration I can call lazygit from neovim

--- setup.sh
+++ setup.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-reldir="$( dirname -- "$0";)";
-cd "$reldir";
-directory="$( pwd )";
+reldir="$(dirname -- "$0")"
+cd "$reldir"
+directory="$(pwd)"
 
-cd "${directory}";
-tmux kill-session -t dotfiles;
-tmux has-session -t dotfiles;
+cd "${directory}"
+tmux kill-session -t dotfiles
+tmux has-session -t dotfiles
 
-tmux new -s dotfiles -n nvim -d;
-tmux send-key -t nvim "nvim ." C-m;
+tmux new -s dotfiles -n nvim -d
+tmux send-key -t nvim "nvim ." C-m
 
-tmux new-window -n lazygit
-tmux send-key -t lazygit "lazygit" C-m;
-
-tmux attach -t dotfiles;
+tmux attach -t dotfiles
