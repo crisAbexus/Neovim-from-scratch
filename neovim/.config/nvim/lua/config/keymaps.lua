7060406774bc18226639f6ad4218061969b2c5c6
cris <ctovar@abexuspr.com>
2023-03-28 11:17:40 -0500

    😄 neovim> override the LEADER-f-t keymap to:
    display the float window terminal with a single line border

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- floating terminal
+-- local Util = require("lazyvim.util")
+-- local term_borders = Util.float_term
+-- local term_borders = '<cmd>lua require("lazyvim.util").float_term(nil, { cwd = require("lazyvim.util").get_root(), border= "single" })<CR>'
+--
+-- vim.keymap.set("n", "<leader>ft", term_borders, {})
+local Util = require("lazyvim.util")
+vim.keymap.set(
+  "n",
+  "<leader>ft",
+  function()
+    Util.float_term(nil, { cwd = Util.get_root(), border= "single" })
+  end,
+  { noremap = true, silent = true, desc = "Resume" }
+)
