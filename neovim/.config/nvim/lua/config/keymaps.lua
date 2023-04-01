9cd99e33a1f757aff3bc888992cf7a41de23d532
cris <ctovar@abexuspr.com>
2023-04-01 05:16:03 -0500

    🪷 neovimn install rest-nvim

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -7,12 +7,25 @@
 -- local term_borders = '<cmd>lua require("lazyvim.util").float_term(nil, { cwd = require("lazyvim.util").get_root(), border= "single" })<CR>'
 --
 -- vim.keymap.set("n", "<leader>ft", term_borders, {})
+
 local Util = require("lazyvim.util")
-vim.keymap.set(
-  "n",
-  "<leader>ft",
-  function()
-    Util.float_term(nil, { cwd = Util.get_root(), border= "single" })
+vim.keymap.set("n", "<leader>ft", function()
+  Util.float_term(nil, { cwd = Util.get_root(), border = "single" })
+end, { noremap = true, silent = true, desc = "Resume" })
+
+local status_ok, rest = pcall(require, "rest-nvim")
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "http",
+  callback = function()
+    local buff = tonumber(vim.fn.expand("<abuf>"), 10)
+    vim.keymap.set("n", "ñrr", rest.run, { noremap = true, buffer = buff })
+    vim.keymap.set(
+      "n",
+      "ñro",
+      '<cmd> lua r=require("rest-nvim");r.setup({result={show_headers=false, show_http_info=false}});r.run()<CR>',
+      { noremap = true, buffer = buff }
+    )
+    --[[ vim.keymap.set("n", "ñrl", rest.last, { noremap = true, buffer = buff }) ]]
+    vim.keymap.set("n", "ñrp", rest.run(true), { noremap = true, buffer = buff })
   end,
-  { noremap = true, silent = true, desc = "Resume" }
-)
+})
