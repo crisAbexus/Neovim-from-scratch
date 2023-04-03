01696e5bb20cf31f289507c9fabf96589a920bc7
cris <ctovar@abexuspr.com>
2023-04-03 16:52:58 -0500

    🌄 neovim>override <leader><space> to find files from cwd

---
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,10 @@
+local Util = require("lazyvim.util")
+
+return {
+  "nvim-telescope/telescope.nvim",
+  cmd = "Telescope",
+  version = false, -- telescope did only one release, so use HEAD for now
+  keys = {
+    { "<leader><space>", Util.telescope("files"), desc = "Find Files (root dir)" },
+  },
+}
