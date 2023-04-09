84de883e08e3df455b36ae98f98e4be1390c6d33
cris <ctovar@abexuspr.com>
2023-04-09 08:24:42 -0500

    🪔 neovim>fix the annoying LazyVim: Parser not available for language "help" error.

--- neovim/.config/nvim/lua/plugins/neo-tree.lua
+++ neovim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,12 +1,36 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      bind_to_cwd = false,
-      follow_current_file = true,
-      filtered_items = {
-        hide_dotfiles = false,
-        hide_gitignored = false,
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = true,
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "vimdoc",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
       },
     },
   },
