6cc22b0aea676fd7e576a922605f961c7cb00f8a
cris <ctovar@abexuspr.com>
2023-04-25 07:29:49 -0500

    🎮 neovim>vimtex>add ejmastnak snippets

--- neovim/.config/VimTeX/lua/plugins/luasnip.lua
+++ neovim/.config/VimTeX/lua/plugins/luasnip.lua
@@ -8,6 +8,7 @@ return {
 
       -- require("luasnip.loaders.from_vscode").lazy_load()
       require("luasnip.loaders.from_lua").load({ paths = "~/.config/snippets/" })
+      require("luasnip.loaders.from_lua").load({ paths = "~/.config/VimTeX/snippets/" })
       require("luasnip").config.setup({ store_selection_keys = "<A-p>" })
 
       -- javascript and react snippets
